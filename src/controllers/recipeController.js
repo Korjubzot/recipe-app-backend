dd3536be15de45582d9f2de1534d4ddda619edb1
Korjubzot <korjubzot@gmail.com>
2023-10-02 20:11:29 +0100

    Fixed some bugs with recipeController

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,15 +1,25 @@
 const db = require("../../db/index");
-const queries = require('../queries')
+const queries = require("../queries");
 
 const createRecipe = async (req, res) => {
-  const { name, cuisine, cooking_time, servings, instructions } = req.body;
+  const { name, cuisine, cooking_time, servings, steps } = req.body;
 
   try {
-    const {
-      rows: [recipe],
-    } = await db.query(queries.selectAllRecipes,
-      [name, cuisine, cooking_time, servings, instructions]
-    );
+    const sql = `
+      INSERT INTO recipes (name, cuisine, cooking_time, servings, steps)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+
+    const values = [name, cuisine, cooking_time, servings, steps];
+
+    const { rows } = await db.query(sql, values);
+
+    if (rows.length === 0) {
+      return res.status(500).json({ error: "Recipe creation failed" });
+    }
+
+    const recipe = rows[0];
     res.status(201).json(recipe);
   } catch (err) {
     console.error("Error creating recipe:", err);
@@ -39,7 +49,6 @@ const getRecipeById = async (req, res) => {
 
     const recipe = result.rows[0];
     res.status(200).json(recipe);
-
   } catch (error) {
     console.error("Error retrieving recipe by ID:", error);
     res.status(500).json({ error: "Internal Server Error" });
