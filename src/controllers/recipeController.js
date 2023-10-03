397f8262a7d9c5979a4408455570ee0ab82297dc
Korjubzot <korjubzot@gmail.com>
2023-10-03 19:10:46 +0100

    Added delete router/controllers

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -2,20 +2,25 @@ const db = require("../../db/index");
 const queries = require("../queries");
 
 const createRecipe = async (req, res) => {
-  const { name, cuisine, cooking_time, servings, ingredients, instructions } = req.body;
+  const { name, cuisine, cooking_time, servings, ingredients, instructions } =
+    req.body;
 
   try {
     const {
       rows: [recipe],
-    } = await db.query(queries.insertRecipe,
-      [name, cuisine, cooking_time, servings, ingredients, instructions]
-    );
+    } = await db.query(queries.insertRecipe, [
+      name,
+      cuisine,
+      cooking_time,
+      servings,
+      ingredients,
+      instructions,
+    ]);
     res.status(201).json(recipe);
   } catch (err) {
     console.error("Error creating recipe:", err);
     res.status(500).json({ error: "Internal server error" });
   }
-
 };
 
 const getRecipe = async (_, res) => {
@@ -41,11 +46,27 @@ const getRecipeById = async (req, res) => {
     const recipe = result.rows[0];
 
     res.status(200).json({ recipe });
-
   } catch (error) {
     console.error("Error retrieving recipe by ID:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-module.exports = { createRecipe, getRecipe, getRecipeById };
+const deleteRecipeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = "DELETE FROM recipes WHERE id = $1";
+    const result = await db.query(query, [id]);
+
+    if (!result.rowCount) {
+      return res.status(404).json({ error: "Recipe not found" });
+    }
+
+    res.status(204).json();
+  } catch (error) {
+    console.error("Error deleting recipe by ID:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { createRecipe, getRecipe, getRecipeById, deleteRecipeById };
--- src/routes/recipeRouter.js
+++ src/routes/recipeRouter.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db/index");
-const { createRecipe, getRecipe, getRecipeById } = require("../controllers/recipeController");
+const {
+  createRecipe,
+  getRecipe,
+  getRecipeById,
+  deleteRecipeById,
+} = require("../controllers/recipeController");
 
 router.post("/recipes", createRecipe);
 
 router.get("/recipes", getRecipe);
 
-router.get("/recipe/:id", getRecipeById);
+router.get("/recipes/:id", getRecipeById);
+
+router.delete("/recipes/:id", deleteRecipeById);
 
 module.exports = router;
